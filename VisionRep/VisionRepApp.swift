03f9ab6c5895efd1ade9af10b37bfc7f1f3d6457
Zheng Pan <zpan1010@gmail.com>
2026-04-29 18:38:32 +0800

    Initial Commit

---
+++ VisionRep/VisionRepApp.swift
@@ -0,0 +1,17 @@
+//
+//  VisionRepApp.swift
+//  VisionRep
+//
+//  Created by Zheng Pan on 2026-04-29.
+//
+
+import SwiftUI
+
+@main
+struct VisionRepApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
